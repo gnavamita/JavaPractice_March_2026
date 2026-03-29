585cbf2701f9d708cef5827d9553f2b1947926c2
gnavamita <gnavamita@gmail.com>
2026-03-29 15:49:10 +0530

    adding operators file

---
+++ src/revision_date290326/Operators.java
@@ -0,0 +1,18 @@
+package revision_date290326;
+
+public class Operators {
+    public static void main(String[] args) {
+        int a = 1;
+        int b= 2;
+        System.out.println("a : "+a + " b : "+b);
+        System.out.println("++a :" +  (++a));
+        System.out.println("b++ :" +  (b++));
+        System.out.println("a!=b " +  (a!=b));
+        System.out.println("a % b " +  (a%b));
+        System.out.println("a-- " +  (a--));
+        System.out.println("--b " +  (--b));
+
+
+
+    }
+}
