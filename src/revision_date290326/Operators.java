6702dd8cd8380561966df56acece8bb4ebabca6f
gnavamita <gnavamita@gmail.com>
2026-03-29 16:07:09 +0530

    adding new program

--- src/revision_date290326/Operators.java
+++ src/revision_date290326/Operators.java
@@ -1,17 +1,21 @@
 package revision_date290326;
 
 public class Operators {
-    public static void main(String[] args) {
-        int a = 1;
-        int b= 2;
-        System.out.println("a : "+a + " b : "+b);
-        System.out.println("++a :" +  (++a));
-        System.out.println("b++ :" +  (b++));
-        System.out.println("a!=b " +  (a!=b));
-        System.out.println("a % b " +  (a%b));
-        System.out.println("a-- " +  (a--));
-        System.out.println("--b " +  (--b));
 
+    void DifferentOperators(int a, int b) {
+
+        System.out.println("a : " + a + " b : " + b);
+        System.out.println("++a :" + (++a));
+        System.out.println("b++ :" + (b++));
+        System.out.println("a!=b " + (a != b));
+        System.out.println("a % b " + (a % b));
+        System.out.println("a-- " + (a--));
+        System.out.println("--b " + (--b));
+    }
+
+    public static void main(String[] args) {
+        Operators ob = new Operators();
+ob.DifferentOperators(2,3);
 
 
     }
