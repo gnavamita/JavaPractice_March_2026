b5fa502a067f8ab869e8f586bac5efa24427357e
gnavamita <gnavamita@gmail.com>
2026-04-19 23:49:19 +0530

    adding more programs

---
+++ src/date_120426/ForzTick.java
@@ -0,0 +1,10 @@
+package date_120426;
+
+public class ForzTick {
+    public static void main(String[] args) {
+        int n;
+        for (n=10; n>0;n--){
+            System.out.println("tick "+n);
+        }
+    }
+}
