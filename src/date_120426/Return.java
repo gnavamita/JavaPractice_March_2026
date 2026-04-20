b44dac1f59c246cbd42e67a857fe1adfab6201b4
gnavamita <gnavamita@gmail.com>
2026-04-20 23:04:42 +0530

    .

---
+++ src/date_120426/Return.java
@@ -0,0 +1,10 @@
+package date_120426;
+
+public class Return {
+    public static void main(String[] args) {
+        boolean t = true;
+        System.out.println("before the return.");
+        if (t)  return;
+        System.out.println("This wont execute");
+    }
+}
