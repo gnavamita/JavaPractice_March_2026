af5bf2a63150f43a213dd1667d3bd50b0f130764
gnavamita <gnavamita@gmail.com>
2026-04-29 19:21:00 +0530

    created new java files on 29 april 2026

---
+++ src/date_280426/NoBody.java
@@ -0,0 +1,11 @@
+package date_280426;
+
+public class NoBody {
+    public static void main(String[] args) {
+        int i,j;
+        i=100;
+        j=200;
+        while (++i<--j);
+        System.out.println("Midpoint is "+i);
+    }
+}
---
+++ src/date_280426/While.java
@@ -0,0 +1,11 @@
+package date_280426;
+
+public class While {
+    public static void main(String[] args) {
+        int n=10;
+        while (n>10){
+            System.out.println("tick "+ n);
+            n--;
+        }
+    }
+}
